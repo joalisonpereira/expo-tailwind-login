a7a10b68a2af9bc4d45e6c0b91ef1e027693da7a
joalisonpereira <joalison.pereira@hotmail.com>
2023-05-25 09:07:47 -0300

    install typescript and tailwind

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 justify-center items-center bg-black">
+      <Text className="font-bold text-white">
+        Hello world, we will make a login app!
+      </Text>
+    </View>
+  );
+}
